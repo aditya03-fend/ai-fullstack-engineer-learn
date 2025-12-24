18fb688c8ffe05c9c5b93553be2d7702b4519865
aditya03-fend <adityaportfolio3@gmail.com>
2025-12-24 15:23:44 +0700

    first commit

---
+++ List, dict, set/set.py
@@ -0,0 +1,18 @@
+# Contoh
+angka = {1, 2, 3, 3, 4}
+print(angka)
+
+
+
+
+# Operasi Set
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a | b)  # union
+print(a & b)  # intersection
+print(a - b)  # difference
+
+
+
+
---
+++ Variabel & Tipe Data/praktik.py
@@ -0,0 +1,16 @@
+
+# Praktik
+nama = "Nama Kamu"
+umur = 20
+tinggi = 170.5
+is_student = True
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Mahasiswa:", is_student)
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(is_student))
